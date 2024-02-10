f7bef6ce9bee17da0d420ea0066bef85e8b28535
Idriss Neumann <neumann.idriss@gmail.com>
2024-02-10 10:32:49 +0000

    Change mirrors

--- ci/mirror.sh
+++ ci/mirror.sh
@@ -5,6 +5,4 @@ REPO_PATH="${PROJECT_HOME}/ansible-portainer-agent/"
 cd "${REPO_PATH}" && git pull origin main || :
 git push github main 
 git push pgitlab main
-git push bitbucket main
-git push froggit main
 exit 0
